b91aa28d4784e4b5c655320295235120c063bd70
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2017-08-23 16:36:48 -0400

    Added langid and calculations for language percentages.

--- lang_analyze.py
+++ lang_analyze.py
@@ -5,11 +5,16 @@ except ImportError:
 
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 from api_keys import *
+import langid
 
 class TwitterLangMix:
     LANG_TAGGED_TWEETS = 0
     TOTAL_TWEETS = 0
     lang_dict = {}
+    langid_dict = {}
+    lang_percentage_dict = {}
+    agree_list = []
+    disagree_list = []
 
     def fetch_stream_to_file(self, tweet_count, filename):
         output_file = open(filename, "w+")
@@ -34,15 +39,33 @@ class TwitterLangMix:
                     continue
                 self.TOTAL_TWEETS += 1
                 self.check_language(tweet)
+        self.solution_file.write("Total number of tweets: %s\n" % self.TOTAL_TWEETS)
+        percent_lang_tagged = format((float(self.LANG_TAGGED_TWEETS) / self.TOTAL_TWEETS) * 100, ".2f")
+        self.solution_file.write("Total number of tweets with language tag: %s (%s%%)\n" % (self.LANG_TAGGED_TWEETS, percent_lang_tagged))
+        self.solution_file.write("Total number of languages provided by Twitter: %s\n" % len(self.lang_dict))
+        self.calculate_language_percentage(self.lang_dict)
+
+    def calculate_language_percentage(self, lang_dict):
+        for lang, count in lang_dict.iteritems():
+            self.lang_percentage_dict[lang] = format((float(count) / self.TOTAL_TWEETS) * 100, ".2f")
 
     def check_language(self, tweet):
+        tweet_text = tweet['text']
+        langid_classified_lang = langid.classify(tweet_text)[0]
+        self.langid_dict[langid_classified_lang] = self.langid_dict.get(langid_classified_lang, 0) + 1
         if 'lang' in tweet:
             self.LANG_TAGGED_TWEETS += 1
             language = tweet['lang']
             self.lang_dict[language] = self.lang_dict.get(language, 0) + 1
+            if language != langid_classified_lang:
+                self.disagree_list.append(tweet)
+            else:
+                self.agree_list.append(tweet)
 
     def run_main(self):
         tweet_file_name = "10000_tweets.txt"
+        solution_file_name = "solution.txt"
+        self.solution_file = open(solution_file_name, "w+")
         self.fetch_stream_to_file(10000, tweet_file_name)
         print "Stream fetched"
         self.process_tweets_in_file(tweet_file_name)

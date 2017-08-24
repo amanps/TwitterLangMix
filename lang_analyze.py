4f0eaf328a832cd84cdb73ccfcddd1abe43b2e5c
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2017-08-24 00:46:36 -0400

    Added bar graph for percent distribution

--- lang_analyze.py
+++ lang_analyze.py
@@ -45,36 +45,49 @@ class TwitterLangMix:
         self.solution_file.write("Total number of tweets with language tag: %s (%s%%)\n" % (self.LANG_TAGGED_TWEETS, percent_lang_tagged))
         self.solution_file.write("Total number of languages provided by Twitter: %s\n" % len(self.lang_dict))
         self.calculate_language_percentage(self.lang_dict)
-        self.build_language_plots()
+        self.build_scatter_line_plot(self.lang_dict, "Languages", "Number of Tweets", "LanguageDistribution.png")
+        self.build_bar_plot(self.lang_percentage_dict, "Percentage", "Languages", "LanguagePercentDistribution.png")
 
-    def build_language_plots(self):
-        lang_list = sorted(self.lang_dict, key = self.lang_dict.get, reverse = True)
-        count_list = sorted(self.lang_dict.values(), reverse = True)
-        lang_range = range(len(lang_list))
+    def build_bar_plot(self, data_dict, xlabel, ylabel, filename):
+        x_list = sorted(data_dict, key = data_dict.get, reverse = True)
+        y_list = sorted(data_dict.values(), reverse = True)
+        x_range = range(len(x_list))
+        #matplot.figure(figsize=(30, 30))
+        matplot.barh(x_range, y_list, align = 'center', alpha = 1)
+        matplot.yticks(x_range, x_list, fontsize=5)
+        matplot.xlabel(xlabel)
+        matplot.ylabel(ylabel)
+        matplot.savefig(filename)
+
+    def build_scatter_line_plot(self, data_dict, xlabel, ylabel, filename):
+        x_list = sorted(data_dict, key = data_dict.get, reverse = True)
+        y_list = sorted(data_dict.values(), reverse = True)
+        x_range = range(len(x_list))
         #matplot.xticks(lang_range, [''] * len(lang_list), rotation=45)
-        matplot.ylabel('Number of Tweets')
-        matplot.xlabel('Language')
-        matplot.scatter(lang_range, count_list)
-        matplot.plot(lang_range, count_list)
+        matplot.ylabel(ylabel)
+        matplot.xlabel(xlabel)
+        matplot.scatter(x_range, y_list)
+        matplot.plot(x_range, y_list)
         alternator = 1
-        for text, xcoord, ycoord in zip(lang_list, lang_range, count_list):
+        for text, xcoord, ycoord in zip(x_list, x_range, y_list):
             factor = 1
             if alternator % 2 == 0:
                 factor = -1.5
             matplot.annotate(text, xy = (xcoord, ycoord), xytext = (factor * 20, factor * 20), textcoords = 'offset points', \
                     arrowprops = dict(arrowstyle = '-', connectionstyle='arc3,rad=0.3'))
             alternator += 1
-        matplot.savefig("LanguageDistribution.png")
+        matplot.savefig(filename)
+        matplot.clf()
 
     def calculate_language_percentage(self, lang_dict):
         for lang, count in lang_dict.iteritems():
-            self.lang_percentage_dict[lang] = format((float(count) / self.TOTAL_TWEETS) * 100, ".2f")
+            self.lang_percentage_dict[lang] = float(format((float(count) / self.TOTAL_TWEETS) * 100, ".2f"))
 
     def check_language(self, tweet):
         tweet_text = tweet['text']
         langid_classified_lang = langid.classify(tweet_text)[0]
         self.langid_dict[langid_classified_lang] = self.langid_dict.get(langid_classified_lang, 0) + 1
-        if 'lang' in tweet:
+        if 'lang' in tweet and tweet['lang'] != 'und':
             self.LANG_TAGGED_TWEETS += 1
             language = tweet['lang']
             self.lang_dict[language] = self.lang_dict.get(language, 0) + 1
